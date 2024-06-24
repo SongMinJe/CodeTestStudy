aa5d0dd9310800597d947a5f47fca6e663935c08
Song MinJe <dlsgur3543@gmail.com>
2024-06-24 12:26:06 +0900

    input

---
+++ BaekJoon5585/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BaekJoon5585
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coin = { 500, 100, 50, 10, 5, 1 };
+            int input = int.Parse(Console.ReadLine());
+            input = 1000 - input;
+            int result = 0;
+
+            for (int i = 0; i < coin.Length; i++)
+            {
+                if (coin[i] <= input)
+                {
+                    result += input / coin[i];
+                    input %= coin[i];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
