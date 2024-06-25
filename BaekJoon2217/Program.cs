6b0db3e4de5abb2b3a59896ea1a55bd097d3ad32
SongMinJe <dlsgur3543@gmail.com>
2024-06-25 13:09:47 +0900

    input

---
+++ BaekJoon2217/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaekJoon2217
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> input = new List<int>();
+            int max = 0;
+            int curMax = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                input.Add(int.Parse(Console.ReadLine()));
+            }
+
+            input.Sort();
+
+            for (int j = 0; j < n; j++)
+            {
+                curMax = input[j] * (n - j);
+                max = curMax > max ? max = curMax : max;
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
