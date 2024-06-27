cc690fd888b37618022ff54586e119aee2b644d2
SongMinJe <dlsgur3543@gmail.com>
2024-06-27 12:31:34 +0900

    input

---
+++ BeakJoon1789/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BeakJoon1789
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long sum = 0;
+            long i = 0;
+            long result = 0;
+            while (sum < n)
+            {
+                i++;
+                sum += i;
+                if (sum > n)
+                {
+                    break;
+                }
+                else
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
