d8df84abdba73409e64f971d1f70637f948dcd30
SongMinJe <dlsgur3543@gmail.com>
2024-06-29 17:16:56 +0900

    input

---
+++ BaekJoon10872/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BaekJoon10872
+{
+    internal class Program
+    {
+        
+        static int Pacto(int n)
+        {
+            int result = 1;
+            if (n > 1)
+            {
+                result = n * Pacto(n - 1);
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Pacto(n));
+        }
+    }
+}
