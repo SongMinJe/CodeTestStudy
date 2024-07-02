39d2be6b05376f68ace3940963201c34d491ba10
SongMinJe <dlsgur3543@gmail.com>
2024-07-02 09:36:35 +0900

    BaekJoon2750

---
+++ BaekJoon2750/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+
+namespace BaekJoon2750
+{
+    internal class Program
+    {
+        static StreamReader reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        static void Main(string[] args)
+        {
+            int n = int.Parse(reader.ReadLine());
+            int[] input = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                input[i] = int.Parse(reader.ReadLine());
+            }
+            Array.Sort(input);
+            for (int i = 0; i < n; i++)
+            {
+                writer.WriteLine(input[i]);
+            }
+            writer.Flush();
+            reader.Close();
+        }
+    }
+}
