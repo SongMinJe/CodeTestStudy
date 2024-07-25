b5fa193dab841c8bdd0736bdf628cbdd1e76a77e
SongMinJe <dlsgur3543@gmail.com>
2024-07-25 13:42:59 +0900

    BaekJoon_24723

---
+++ BaekJoon24723/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace BaekJoon24723
+{
+    internal class Program
+    {
+        static StreamReader reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        static void Main(string[] args)
+        {
+            int n = int.Parse(reader.ReadLine());
+            writer.WriteLine(Math.Pow(2, n));
+            reader.Close();
+            writer.Flush();
+        }
+    }
+}
