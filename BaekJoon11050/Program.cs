da68a158639c09c0c7f5845cc7f276b181a896ad
SongMinJe <dlsgur3543@gmail.com>
2024-07-29 12:28:12 +0900

    BaekJoon_11050

---
+++ BaekJoon11050/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace BaekJoon11050
+{
+    internal class Program
+    {
+        static StreamReader reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        static void Main(string[] args)
+        {
+            int[] inputs = Array.ConvertAll(reader.ReadLine().Split(' '), int.Parse);
+            int n = inputs[0];
+            int k = inputs[1];
+            int result = Pacto(n) / (Pacto(n - k) * Pacto(k));
+            writer.WriteLine(result);
+            reader.Close();
+            writer.Flush();
+        }
+        static int Pacto(int num)
+        {
+            if (num <= 1)
+            {
+                return 1;
+            }
+            return num * Pacto(num - 1);
+        }
+    }
+}
