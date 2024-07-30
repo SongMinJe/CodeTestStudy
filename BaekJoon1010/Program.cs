6b8104235c54ce651e390240614a37bcba030f63
SongMinJe <dlsgur3543@gmail.com>
2024-07-30 13:35:25 +0900

    BaekJoon_1010

---
+++ BaekJoon1010/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+namespace BaekJoon1010
+{
+    internal class Program
+    {
+        static StreamReader reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        static void Main(string[] args)
+        {
+            int caseNum = int.Parse(reader.ReadLine());
+            BigInteger result = 0;
+            result = 0;
+            for (int i = 0; i < caseNum; i++)
+            {
+                int[] site = Array.ConvertAll(reader.ReadLine().Split(' '), int.Parse);
+                int n = Math.Min(site[0], site[1]);
+                int m = Math.Max(site[0], site[1]);
+                result = Pacto(m) / (Pacto(n) * Pacto(m - n));
+                writer.WriteLine(result);
+            }
+            reader.Close();
+            writer.Flush();
+        }
+
+        static BigInteger Pacto(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            return n * Pacto(n - 1);
+        }
+    }
+}
